a6b27ebc457b060356ad0cc0123883acd9747313
WindMatter <44716748+WindMatter@users.noreply.github.com>
2025-03-07 14:05:20 +0900

    Add files via upload

---
+++ common.js
@@ -0,0 +1,31 @@
+$.fn.serializeObject = function() {
+    "use strict"
+    let result = {}
+    let extend = function(i, element) {
+        let node = result[element.name]
+        if ("undefined" !== typeof node && node !== null) {
+            if ($.isArray(node)) {
+                node.push(element.value)
+            } else {
+                result[element.name] = [node, element.value]
+            }
+        } else {
+            result[element.name] = element.value
+        }
+    }
+
+    $.each(this.serializeArray(), extend)
+    return result
+}
+
+function sortJsonData(data, key, order = 'asc') {
+    return data.sort((a, b) => {
+        if (a[key] < b[key]) {
+            return order === 'asc' ? -1 : 1;
+        }
+        if (a[key] > b[key]) {
+            return order === 'asc' ? 1 : -1;
+        }
+        return 0;
+    });
+}
